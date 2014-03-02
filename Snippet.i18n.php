0be15f4a954f8f939a14423f6c87bfdf1d736453
George Barnick <george.barnick@brickcraft.me>
2014-03-02 11:57:35 -0500

    Adding German (Deutsch) internationalisation

--- Snippet.i18n.php
+++ Snippet.i18n.php
@@ -2,6 +2,16 @@
 
 $messages = array();
 
+/** English (English)
+ * @author UltrasonicNXT
+ */
 $messages['en'] = array(
-        'snippet-desc' => "An API extension returning a snippet of a page from a search"
+        'snippet-desc' => 'An API extension returning a snippet of a page from a search'
+);
+
+/** German (Deutsch)
+ * @author ToaMeiko
+ */
+$messages['de'] = array(
+        'snippet-desc' => 'Eine API-Erweiterung Rückkehr einen Ausschnitt einer Seite aus einem Such'
 );
